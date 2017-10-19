b9792c480458a0682cdeb4f463d8827288a89cb3
clement <clement@keiruaprod.fr>
2017-10-19 21:02:59 +0200

    minor improvements on level13

--- set2/level13_ecb_cutpaste.py
+++ set2/level13_ecb_cutpaste.py
@@ -1,5 +1,22 @@
 import unittest
 
+class TestParseCookie(unittest.TestCase):
+    def test_querystring_parse(self):
+        s = "foo=bar&baz=qux&zap=zazzle"
+        self.assertEqual({'baz': 'qux', 'foo': 'bar', 'zap': 'zazzle'}, querystring_parse(s))
+        self.assertEqual({'a':'b'}, querystring_parse('a=b'))
+        self.assertEqual({}, querystring_parse(''))
+        self.assertEqual({}, querystring_parse('a'))
+
+    def test_profile_for(self):
+        self.assertEqual("email=bidou@plop.com&uid=10&role=user", profile_for("bidou@plop.com"))
+
+    def test_profile_for_error(self):
+        with self.assertRaises(ValueError):
+            profile_for("abc&def")
+        with self.assertRaises(ValueError):
+            profile_for("foo=bar")
+
 def querystring_parse(s):
     d = {}
     kvList = s.split('&')
@@ -10,18 +27,10 @@ def querystring_parse(s):
     return d
 
 def profile_for(email):
+    if ('&' in email or '=' in email):
+        raise ValueError("& and = are not allowed in the input string")
     return "email={}&uid=10&role=user".format(email)
 
-class TestParseCookie(unittest.TestCase):
-    def test_nominal(self):
-        s = "foo=bar&baz=qux&zap=zazzle"
-        self.assertEqual({'baz': 'qux', 'foo': 'bar', 'zap': 'zazzle'}, querystring_parse(s))
-        self.assertEqual({'a':'b'}, querystring_parse('a=b'))
-        self.assertEqual({}, querystring_parse(''))
-        self.assertEqual({}, querystring_parse('a'))
-
-    def test_profile_for(self):
-        self.assertEqual("email=bidou@plop.com&uid=10&role=user", profile_for("bidou@plop.com"))
 
 if __name__ == '__main__':
     unittest.main()
